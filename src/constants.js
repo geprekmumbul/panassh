3efdf38776ce1f7a3d80f6d31662c0d03397d0e4
carolinekencana <126485207+carolinekencana@users.noreply.github.com>
2024-06-25 21:32:29 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const SPACE_BG = "//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png";
 export const GLOBE_TEXTURE = "//unpkg.com/three-globe/example/img/earth-blue-marble.jpg";
-export const GH_CONT = "//github.com/geprekmumbul";
+export const GH_CONT = "//github.com/geprekmumbul/panassh";
 export const EMAIL_CONT = "mailto:blackops.psoa@gmail.com";
 export const LINKEDIN_CONT = "";
